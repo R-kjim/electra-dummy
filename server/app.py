9b2367eabaf0857216649e5f928ec7581b2c7491
R-kjim <rkayjim@gmail.com>
2024-10-23 21:26:31 +0300

    deployments

--- server/app.py
+++ server/app.py
@@ -547,5 +547,6 @@ class VoteResource(Resource):
 
 api.add_resource(VoteResource,'/vote')
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    port = int(os.environ.get("PORT", 5555))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
